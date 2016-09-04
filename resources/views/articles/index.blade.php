83d61c48c470b34545e12ef7d976a38092baeb21
happylwp <18804207538@163.com>
2016-09-04 10:52:14 +0800

    add index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -8,31 +8,32 @@
 		<div class="col-md-4">
 			<h1>Articles</h1>
 			<h5>Page {{ $articles->currentPage() }} of {{ $articles->lastPage() }}</h5>
+		    @if(Session::has('error'))
+			    <div class="alert alert-success">{{ Session::get('error') }}</div>
+		    @endif
 		</div>
 
 		<div class="col-md-6">
 			@foreach($articles as $article)
-			    @if(Session::has('error'))
-				    <div class="alert alert-success">{{ Session::get('error') }}</div>
-			    @endif
 				<div class="list-group">
 				  <a href="{{ url('articles', $article->id) }}" class="list-group-item">
 				    <h4 class="list-group-item-heading">Author: {{ $article->username }}
-						<span class="label label-info pull-right">publised-time: {{ $article->published_at }}</span>
+						<span class="label label-info pull-right">created-time: {{ $article->created_at }}</span>
 				    </h4>
 				    <p class="list-group-item-text">
 				    	<h3>{{ $article->title }}</h3>
-						<a href="/articles/love/{{ $article->id }}" onclick="event.preventDefault();
-                                                 document.getElementById('love-form').submit();"><span class="label label-danger pull-right"><i class="fa fa-heart-o" aria-hidden="true"></i>{{ $article->love }}</span>
-</a>
-                        <form id="love-form" action="/articles/love/{{ $article->id }}" method="POST" style="display: none;">
+						<span class="label label-primary">readtimes: {{ $article->readtimes }}</span>
+
+                        <form action="{{ route('love') }}" method="POST">
                             {{ csrf_field() }}
+                            <input type="hidden" name="id" value="{{ $article->id }}">
+							<button type="submit" class="btn btn-sm btn-danger pull-right"><i class="fa fa-heart-o" aria-hidden="true"></i>{{ $article->love }}</button>
                         </form>
-						<a href="/articles/unLove/{{ $article->id }}" onclick="event.preventDefault();
-                                                 document.getElementById('unlove-form').submit();"><span class="label label-success pull-right"><i class="fa fa-thumbs-down" aria-hidden="true"></i></span>
-</a>
-                        <form id="unlove-form" action="/articles/unLove/{{ $article->id }}" method="POST" style="display: none;">
+
+                        <form action="{{ route('unlove') }}" method="POST">
                             {{ csrf_field() }}
+							<input type="hidden" name="id" value="{{ $article->id }}">
+							<button type="submit" class="btn btn-sm btn-success pull-right"><i class="fa fa-thumbs-down" aria-hidden="true"></i>{{ $article->unLove }}</button>
                         </form>
 				    </p>
 				  </a>
