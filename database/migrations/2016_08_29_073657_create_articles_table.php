e71b9d9001138dea2fbc1af36c72c85c218846e5
happylwp <18804207538@163.com>
2016-09-04 11:10:35 +0800

    new

--- database/migrations/2016_08_29_073657_create_articles_table.php
+++ database/migrations/2016_08_29_073657_create_articles_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
 class CreateArticlesTable extends Migration
 {
     /**
@@ -15,29 +13,25 @@ class CreateArticlesTable extends Migration
     {
        Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
-
             /**
              * Specify the user ID
              */
-
             $table->string('username');
             $table->string('title');
             $table->longText('content');
-            $table->integer('love')->default(0);
+            $table->integer('love')->unsigned()->default(0);
+            $table->integer('unLove')->unsigned()->default(0);
             $table->timestamp('published_at');
             $table->timestamps();
-
             /**
              *  Foreign key generation,user_id and the article_id connect
              */
-
             $table->foreign('username')
                   ->references('name')
                   ->on('users')
                   ->onDelete('cascade');
         });
     }
-
     /**
      * Reverse the migrations.
      *
