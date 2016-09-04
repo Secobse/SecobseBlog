257d3faef101f7b54ccdbf83d3e6644f583ac0a0
G1enY0ung <g1enyy0ung@gmail.com>
2016-09-04 10:40:26 +0800

    add Disqus, no config Disqus

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -26,5 +26,27 @@
 	<button class="btn btn-primary" onclick="history.go(-1)">
 		« Back
 	</button>
+
+	<div id="disqus_thread"></div>
+	<script>
+
+	/**
+	 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+	 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables */
+	/*
+	var disqus_config = function () {
+	    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+	    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+	};
+	*/
+	(function() { // DON'T EDIT BELOW THIS LINE
+	    var d = document, s = d.createElement('script');
+	    s.src = '//secobse.disqus.com/embed.js';
+	    s.setAttribute('data-timestamp', +new Date());
+	    (d.head || d.body).appendChild(s);
+	})();
+	</script>
+	<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+	<script id="dsq-count-scr" src="//secobse.disqus.com/count.js" async></script>
 </div>
 @endsection
