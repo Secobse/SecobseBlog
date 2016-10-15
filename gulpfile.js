986027affc1aa854bcbf8ec912d9d4392607256f
Gasbylei <fantonglei@vip.qq.com>
2016-10-15 11:52:01 +0800

    add select2

--- gulpfile.js
+++ gulpfile.js
@@ -15,12 +15,16 @@ require('laravel-elixir-vue');
 
 elixir(mix => {
     mix.sass(['app.scss', 'content.scss', 'offer.scss'])
-       .sass('welcome.scss', 'public/css')
-       .sass('home.scss', 'public/css')
-       .copy('node_modules/font-awesome/fonts', 'public/fonts')
-       .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap', 'public/fonts/bootstrap')
-       .copy('node_modules/simplemde/dist/simplemde.min.css', 'public/css/simplemde.min.css')
-       .copy('node_modules/simplemde/dist/simplemde.min.js', 'public/js/simplemde.min.js')
-       .webpack('slide.js', 'public/js')
-       .webpack('app.js');
-});
+    .sass('welcome.scss', 'public/css')
+    .sass('home.scss', 'public/css')
+    .sass('select2.sass', 'public/css')
+    .sass('select2-bootstrap.sass', 'public/css')
+    .copy('node_modules/font-awesome/fonts', 'public/fonts')
+    .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap', 'public/fonts/bootstrap')
+    .copy('node_modules/simplemde/dist/simplemde.min.css', 'public/css/simplemde.min.css')
+    .copy('node_modules/simplemde/dist/simplemde.min.js', 'public/js/simplemde.min.js')
+    .webpack('slide.js', 'public/js')
+    .webpack('select2,js', 'public/js')
+    .webpack('app.js');
+})
+;
